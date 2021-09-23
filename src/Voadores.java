802177bb94d9dac22e90f8ae61714b47076c24a1
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-23 14:24:13 -0300

    modelagem classe Voadores

--- src/Voadores.java
+++ src/Voadores.java
@@ -1,2 +1,20 @@
-public class Voadores {
+public class Voadores extends Inimigos {
+
+    private double danoTiro;
+
+    public Voadores(String nome, double vida, double danoTiro) {
+        super(nome, vida);
+        this.danoTiro = danoTiro;
+    }
+
+    public double aplicarDano(){
+        return danoTiro;
+    }
+
+    public double getDanoTiro() {
+        return danoTiro;
+    }
 }
+
+
+
