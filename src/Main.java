7a79c444aa0c0227979e632db9c6a81b63ac7f47
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-23 14:54:04 -0300

    instanciada classe Guerreiros e invocados métodos própios e herdados

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         heroi1.receberDano(350);
         System.out.println(heroi1.exibirVida());
 
+
         //instanciando classe Magos e invocando métodos
 
         Magos mago1 = new Magos("Merlin", 1000, 250);
@@ -23,6 +24,21 @@ public class Main {
         mago1.trocarNome("Dumbledore");
         System.out.println(mago1.getNome());
         System.out.println(mago1.exibirVida());
+
+        //instanciando classe Guerreiros e invocando métodos
+
+        Guerreiros guerreiro1 = new Guerreiros("Conan", 900, 380);
+        System.out.println(guerreiro1.getVida());
+        guerreiro1.aplicarDano();
+        System.out.println(guerreiro1.getNome());
+        mago1.trocarNome("Coisa");
+        System.out.println(guerreiro1.getNome());
+        System.out.println(guerreiro1.exibirVida());
+        guerreiro1.receberDano(125);
+        System.out.println(guerreiro1.exibirVida());
+
+        //instanciando classe Guerreiros e invocando métodos
+
 
 
 
