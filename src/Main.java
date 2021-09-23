79abc37c71c4d9c86b2602191309af475eb5236d
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-23 14:37:11 -0300

    Instanciada classe Herois e invocados métodos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Instanciando classe Heroi e invocando métodos
+        Heroi heroi1 = new Heroi("Batman", 1000);
+
+        System.out.println(heroi1.getNome());
+        heroi1.trocarNome("Homem-Aranha");
+        System.out.println(heroi1.getNome());
+        System.out.println(heroi1.exibirVida());
+        heroi1.receberDano(350);
+        System.out.println(heroi1.exibirVida());
+
+
+
+
     }
 }
