03bed5af4473e61916643b88006005c07eaed496
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-23 14:18:50 -0300

    modelagem classe Rasteiros

--- src/Rasteiros.java
+++ src/Rasteiros.java
@@ -1,2 +1,18 @@
-public class Rasteiros {
+public class Rasteiros extends Inimigos {
+
+    private double danoEspinhos;
+
+    public double aplicarDano(){
+        return danoEspinhos;
+    }
+
+    public Rasteiros(String nome, double vida, double danoEspinhos) {
+        super(nome, vida);
+        this.danoEspinhos = danoEspinhos;
+    }
+
+    public double getDanoEspinhos() {
+        return danoEspinhos;
+    }
+
 }
