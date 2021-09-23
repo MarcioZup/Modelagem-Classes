22ae1c8c0910b8665b71fd85194282ad03df104b
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-23 13:26:53 -0300

    Modelagem da classe Heroi

--- src/Heroi.java
+++ src/Heroi.java
@@ -1,2 +1,33 @@
 public class Heroi {
+    private String nome;
+    private double vida;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+   public Heroi(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public void trocarNome(String nome){
+        this.nome = nome;
+    }
+
+    public void receberDano(double dano){
+        this.vida -= dano;
+    }
+
+    public double exibirVida(){
+        return vida;
+    }
 }
